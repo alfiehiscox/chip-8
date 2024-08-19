ca0ee8cec0cacdae0bf4a789bf610b9137f66fd5
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-08-19 18:08:01 +0100

    draw pixel with error handling

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Chip 8 Emulation in Zig
 
-A simple Chip 8 emulator written in zig.
+A simple Chip 8 emulator written in zig (0.14.0).
+
+## Install
+
+```
+git clone https://github.com/alfiehiscox/chip-8.git
+cd chip-8
+zig build run
+```
 
 ## Resources
+
 [Guide](https://tobiasvl.github.io/blog/write-a-chip-8-emulator/)
+
 [raylib](https://www.raylib.com/cheatsheet/cheatsheet.html)
--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,51 @@
 const std = @import("std");
 const raylib = @import("raylib");
 
+const FPS = 60;
+
+// Original Chip8 Screen was 64x32 Pixels.
+const SCREEN_WIDTH = 64;
+const SCREEN_HEIGHT = 32;
+
+// Obviously we're going to scale 10x;
+const SCALAR = 10;
+
+const DrawError = error{OUTOFBOUNDS};
+
+// Draws a pixel on the screen.
+// x is 0..SCREEN_WIDTH non-inclusive.
+// y is 0..SCREEN_HEIGHT non-inclusive.
+// Errors if x or y are out of bounds.
+fn drawPixel(x: u16, y: u16) DrawError!void {
+    if (x < 0 or x > SCREEN_WIDTH - 1) return error.OUTOFBOUNDS;
+    if (y < 0 or y > SCREEN_HEIGHT - 1) return error.OUTOFBOUNDS;
+
+    raylib.drawRectangle(
+        x * SCALAR,
+        y * SCALAR,
+        1 * SCALAR,
+        1 * SCALAR,
+        raylib.Color.black,
+    );
+}
+
+// Simple Error Screen
+fn drawError(err: [*:0]const u8) void {
+    raylib.clearBackground(raylib.Color.black);
+    raylib.drawText("Error:", 2 * SCALAR, 5 * SCALAR, 20, raylib.Color.red);
+    raylib.drawText(err, 2 * SCALAR, 10 * SCALAR, 20, raylib.Color.red);
+}
+
 pub fn main() !void {
-    // raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
-    raylib.initWindow(800, 600, "Hello world");
+    raylib.initWindow(SCREEN_WIDTH * SCALAR, SCREEN_HEIGHT * SCALAR, "Chip8");
     defer raylib.closeWindow();
-    raylib.setTargetFPS(60);
+    raylib.setTargetFPS(FPS);
 
     while (!raylib.windowShouldClose()) {
         raylib.beginDrawing();
         defer raylib.endDrawing();
 
         raylib.clearBackground(raylib.Color.white);
-        raylib.drawFPS(10, 10);
-
-        raylib.drawText("Hello, world!", 100, 100, 20, raylib.Color.light_gray);
+        try drawPixel(0, 0);
     }
 }
