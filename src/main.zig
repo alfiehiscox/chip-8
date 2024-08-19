afcb068b6fd22f2f2b4bacf83ccacd9e7b889463
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-08-19 15:13:27 +0100

    basic raylib

--- src/main.zig
+++ src/main.zig
@@ -1,33 +1,19 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
+const raylib = @import("raylib");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    // raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
+    raylib.initWindow(800, 600, "Hello world");
+    defer raylib.closeWindow();
+    raylib.setTargetFPS(60);
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    while (!raylib.windowShouldClose()) {
+        raylib.beginDrawing();
+        defer raylib.endDrawing();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+        raylib.clearBackground(raylib.Color.white);
+        raylib.drawFPS(10, 10);
 
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    // Try passing `--fuzz` to `zig build` and see if it manages to fail this test case!
-    const input_bytes = std.testing.fuzzInput(.{});
-    try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input_bytes));
+        raylib.drawText("Hello, world!", 100, 100, 20, raylib.Color.light_gray);
+    }
 }
