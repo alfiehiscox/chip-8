ab0ac3ce76abe0f8ad4bcd31aeb0f51fef9720ce
Alfie Hiscox <alfie.hiscox@gmail.com>
2024-08-20 23:49:08 +0100

    IBM Logo Landmark

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 const raylib = @import("raylib");
 
 const FPS = 60;
+const BACKGROUND_COLOR = raylib.Color.black;
 
 // Original Chip8 Screen was 64x32 Pixels.
 const SCREEN_WIDTH = 64;
 const SCREEN_HEIGHT = 32;
+const DIMS = SCREEN_WIDTH * SCREEN_HEIGHT;
+
+// Pixel Buffer
+var SCREEN: [DIMS]u8 = [_]u8{0} ** DIMS;
 
 // Obviously we're going to scale 10x;
 const SCALAR = 10;
@@ -45,6 +50,8 @@ var VC: u8 = 0;
 var VD: u8 = 0;
 var VE: u8 = 0;
 var VF: u8 = 0;
+
+var global_err: ?[*:0]u8 = null;
 
 // Font
 const FONT: [80]u8 = [80]u8{
@@ -86,6 +93,12 @@ const Instruction = union(enum) {
     },
 };
 
+const EmulatorError = error{
+    OUT_OF_MEMORY,
+    UNKNOWN_INSTRUCTION,
+    UNKNOWN_REGISTER,
+};
+
 fn initEmulator(program: []u8) void {
     // Load the font into main memory
     std.mem.copyForwards(u8, MEM[0x050..0x0A0], FONT[0..]);
@@ -95,17 +108,144 @@ fn initEmulator(program: []u8) void {
     std.mem.copyForwards(u8, MEM[PC .. PC + program.len], program);
 }
 
-fn fetchInstruction() !Instruction {
+// Parse the currently pointed at instruction.
+// [See here](https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#execute)
+// for opcode descriptions.
+fn fetchInstruction() EmulatorError!Instruction {
+    if (PC + 2 > MEM.len) return EmulatorError.OUT_OF_MEMORY;
+
     // Get a 2 byte slice of MEM at PC
+    const slice = MEM[PC..(PC + 2)];
+    PC += 2;
+
+    // Convert into a u16 for easy masking
+    const opcode = @as(u16, @intCast(slice[0])) << 8 | @as(u16, @intCast(slice[1]));
+
     // Mask off the first nibble and switch on it
-    // Depending on the above parse the rest of the instruction
-    // Create the Instruction member and return
-    // Error on EoF
-    return error.OUT_OF_BOUNDS;
+    const category: u16 = opcode & 0xF000;
+
+    // Depending on the above parse the rest of the instruction.
+    const instruction: Instruction = switch (category) {
+        0x0000 => Instruction{ .CLEAR_SCREEN = {} },
+        0x1000 => Instruction{ .JUMP = opcode & 0x0FFF },
+        0x6000 => Instruction{ .SET_REGISTER = .{
+            .register = @as(u8, @intCast((opcode & 0x0F00) >> 8)),
+            .value = @as(u8, @intCast(opcode & 0x00FF)),
+        } },
+        0x7000 => Instruction{ .ADD_REGISTER = .{
+            .register = @as(u8, @intCast((opcode & 0x0F00) >> 8)),
+            .value = @as(u8, @intCast(opcode & 0x00FF)),
+        } },
+        0xA000 => Instruction{ .SET_INDEX = opcode & 0x0FFF },
+        0xD000 => Instruction{ .DRAW = .{
+            .register_x = @as(u8, @intCast((opcode & 0x0F00) >> 8)),
+            .register_y = @as(u8, @intCast((opcode & 0x00F0) >> 4)),
+            .size = @as(u8, @intCast(opcode & 0x000F)),
+        } },
+        else => {
+            std.debug.print("Unknown instruction '{X}'\n", .{opcode});
+            return EmulatorError.UNKNOWN_INSTRUCTION;
+        },
+    };
+
+    return instruction;
 }
 
-fn executeInstruction(_: Instruction) !void {
-    return error.OUT_OF_BOUNDS;
+// Manipulate the above variables based on Instruction.
+fn executeInstruction(instruction: Instruction) !void {
+    switch (instruction) {
+        Instruction.CLEAR_SCREEN => |_| raylib.clearBackground(BACKGROUND_COLOR),
+        Instruction.JUMP => |addr| PC = addr,
+        Instruction.SET_REGISTER => |v| {
+            _ = switch (v.register) {
+                0x00 => V0 = v.value,
+                0x01 => V1 = v.value,
+                0x02 => V2 = v.value,
+                0x03 => V3 = v.value,
+                0x04 => V4 = v.value,
+                0x05 => V5 = v.value,
+                0x06 => V6 = v.value,
+                0x07 => V7 = v.value,
+                0x08 => V8 = v.value,
+                0x09 => V9 = v.value,
+                0x0A => VA = v.value,
+                0x0B => VB = v.value,
+                0x0C => VC = v.value,
+                0x0D => VD = v.value,
+                0x0E => VE = v.value,
+                0x0F => VF = v.value,
+                else => return EmulatorError.UNKNOWN_REGISTER,
+            };
+        },
+        Instruction.ADD_REGISTER => |v| {
+            // We allow for wrapping back around to 0 from 255.
+            _ = switch (v.register) {
+                0x00 => V0 = V0 +% v.value,
+                0x01 => V1 = V1 +% v.value,
+                0x02 => V2 = V2 +% v.value,
+                0x03 => V3 = V3 +% v.value,
+                0x04 => V4 = V4 +% v.value,
+                0x05 => V5 = V5 +% v.value,
+                0x06 => V6 = V6 +% v.value,
+                0x07 => V7 = V7 +% v.value,
+                0x08 => V8 = V8 +% v.value,
+                0x09 => V9 = V9 +% v.value,
+                0x0A => VA = VA +% v.value,
+                0x0B => VB = VB +% v.value,
+                0x0C => VC = VC +% v.value,
+                0x0D => VD = VD +% v.value,
+                0x0E => VE = VE +% v.value,
+                0x0F => VF = VF +% v.value,
+                else => return EmulatorError.UNKNOWN_REGISTER,
+            };
+        },
+        Instruction.SET_INDEX => |v| IN = v,
+        Instruction.DRAW => |v| {
+            const x = @as(u16, try getRegisterValue(v.register_x) % SCREEN_WIDTH);
+            const y = @as(u16, try getRegisterValue(v.register_y) % SCREEN_HEIGHT);
+            VF = 0x00;
+
+            for (0..v.size) |n| {
+                if ((y + n) > SCREEN_HEIGHT) continue;
+                const data = MEM[IN + n];
+                for (0..8) |i| {
+                    if ((x + i) > SCREEN_WIDTH) continue;
+                    const bit_index = @as(u3, @intCast(7 - i));
+                    const bit = (data >> bit_index) & 1;
+                    if (bit == 1) {
+                        if (isPixelOn(@as(u16, @intCast(x + i)), @as(u16, @intCast(y + n)))) {
+                            turnPixelOff(@as(u16, @intCast(x + i)), @as(u16, @intCast(y + n)));
+                            VF = 1;
+                        } else {
+                            turnPixelOn(@as(u16, @intCast(x + i)), @as(u16, @intCast(y + n)));
+                        }
+                    }
+                }
+            }
+        },
+    }
+}
+
+fn getRegisterValue(register: u8) EmulatorError!u8 {
+    return switch (register) {
+        0x00 => V0,
+        0x01 => V1,
+        0x02 => V2,
+        0x03 => V3,
+        0x04 => V4,
+        0x05 => V5,
+        0x06 => V6,
+        0x07 => V7,
+        0x08 => V8,
+        0x09 => V9,
+        0x0A => VA,
+        0x0B => VB,
+        0x0C => VC,
+        0x0D => VD,
+        0x0E => VE,
+        0x0F => VF,
+        else => return EmulatorError.UNKNOWN_REGISTER,
+    };
 }
 
 // ======== Stack Functions =======
@@ -144,9 +284,40 @@ fn drawPixel(x: u16, y: u16) DrawError!void {
     );
 }
 
+fn drawScreen() DrawError!void {
+    for (0.., SCREEN) |i, pixel| {
+        const x = @as(i32, @intCast(i % SCREEN_WIDTH));
+        const y = @as(i32, @intCast(i / SCREEN_WIDTH));
+
+        raylib.drawFPS(20, 20);
+        raylib.drawRectangle(
+            x * SCALAR,
+            y * SCALAR,
+            1 * SCALAR,
+            1 * SCALAR,
+            if (pixel == 1) raylib.Color.white else raylib.Color.black,
+        );
+    }
+}
+
+fn turnPixelOn(x: u16, y: u16) void {
+    const i = y * SCREEN_WIDTH + x;
+    SCREEN[i] = 1;
+}
+
+fn turnPixelOff(x: u16, y: u16) void {
+    const i = y * SCREEN_WIDTH + x;
+    SCREEN[i] = 0;
+}
+
+fn isPixelOn(x: u16, y: u16) bool {
+    const i = y * SCREEN_WIDTH + x;
+    return SCREEN[i] == 1;
+}
+
 // Simple Error Screen
 fn drawError(err: [*:0]const u8) void {
-    raylib.clearBackground(raylib.Color.black);
+    raylib.clearBackground(BACKGROUND_COLOR);
     raylib.drawText("Error:", 2 * SCALAR, 5 * SCALAR, 20, raylib.Color.red);
     raylib.drawText(err, 2 * SCALAR, 10 * SCALAR, 20, raylib.Color.red);
 }
@@ -165,7 +336,7 @@ pub fn main() !void {
 
     initEmulator(buf[0..read]);
 
-    var err: ?[*:0]const u8 = null;
+    // var err: ?[*:0]const u8 = null;
 
     while (!raylib.windowShouldClose()) {
         // Decrement Timers: Not sure of a way to do this
@@ -176,21 +347,19 @@ pub fn main() !void {
         raylib.beginDrawing();
         defer raylib.endDrawing();
 
-        // Display Error
-        if (err) |value| {
-            drawError(value);
+        // Render Error
+        if (global_err) |msg| {
+            drawError(msg);
             continue;
         }
 
         // Fetch Instruction
-        const instruction = fetchInstruction() catch {
-            err = "fetch error";
-            continue;
-        };
+        const instruction = try fetchInstruction();
 
         // Execute Instruction
-        executeInstruction(instruction) catch {
-            err = "execute error";
-        };
+        try executeInstruction(instruction);
+
+        // Draw the Screen
+        try drawScreen();
     }
 }
